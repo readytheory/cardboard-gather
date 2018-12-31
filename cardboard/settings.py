1109b49bd374de4a8b288ca6b397f0943d1d7bc2
levin <levin@readytheory.com>
2018-12-30 22:07:14 -0600

    css, staticfiles

--- cardboard/settings.py
+++ cardboard/settings.py
@@ -116,17 +116,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = decouple_config('STATIC_ROOT')
+STATIC_URL = decouple_config('STATIC_URL')
