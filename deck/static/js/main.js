641cfad71df437649f6e4772cd01c20d973c64e4
levin <levin@readytheory.com>
2019-01-03 23:41:30 -0600

    vue tutorial

--- deck/static/js/main.js
+++ deck/static/js/main.js
@@ -1,7 +1,10 @@
 var app= new Vue({
     el: '#app',
     data: {
-	product: 'Socks'
+	product: 'Face Hider',
+	description: 'Be seen. . . but don\'t ',
+	image: '/cardboard_static/green_b.jpg',
+	image_url: 'https://www.airrattle.com/Tactical-Balaclava-OD-Green-p/5779.htm',
     }
 })
 
